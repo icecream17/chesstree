5b426565cd299f9920527d3f68c46fc0796af21b
steven nguyen <nguyeste008@students.garlandisd.net>
2022-05-13 15:41:31 +0000

    automate! resolves #1 !!!!!!!!!!!!!!!!!!!!!!

--- README.md
+++ README.md
@@ -22,19 +22,18 @@ Technically, with the "from" lines, the FEN is redundant, since you have the lis
 Maybe I should remove them
 
 And the grammar is:
-```js
+```rust
+/tablebase
+zero or one /draw reason
 #Id
-FEN
-
-from #Id
-/move/tablebase/ #Id
-draw by /draw reason/
-[w/b] wins!
-comment [any text]
+Id..Id
+zero or more /tablebase
+zero or more /equivalent
 ```
 
-`/draw reason/`:
-```
+`/draw reason`:
+
+```txt
 dead position
 insufficient material
 stalemate
@@ -42,102 +41,51 @@ threefold repetition
 fifty moves
 ```
 
-`/move/` grammar:
-```js
-// Here, all of the fields surrounded by [square brackets like this] are optional
-// Try to ***use the least amount of fields possible**!!!
-move =
-   [piece] [captureInfo] [fromToInfo] [promotionIndicator] [checkIndicator] [checkmateIndicator]
-   [piece] [captureInfo] [fromToInfo] [promotionPiece] [checkIndicator] [checkmateIndicator]
+`/tablebase`:
 
-captureInfo =
-   captureIndicator [piece]
-
-captureIndicator = "x"
-promotionIndicator = "="
-checkIndicator = "+"
-checkmateIndicator = "#"
-
-// You cannot have "fromRank" without "toRank" (so cc is possible, but not c1 or c by itself)
-// You cannot have "fromFile" without "toFile" or "toRank" (so 1c and 12 are possible, but not 1 by itself)
-fromToInfo =
-   [fromRank] [fromFile] toRank [toFile]
-   [fromFile] toFile
-
-piece =
-   PBNRQK
-file =
-   abcdefgh
-rank =
-   12345678
-
-// Example:
-// Consider (exc1=Q#). How is it represented in this optimized format?
-
-// If the move is forced, the best representation is the empty string (!!!!)
-// Else:
-//  If there's only 1 pawn move
-P
-//  If there's only 1 move that captures
-x
-//  If there's only 1 move that goes to rank 1
-1
-// .....
-//  If there's only 1 move that checks (redundant in this case)
-+
-//  If there's only 1 move that checkmates
-#
-//  If there's only 1 pawn move that captures
-Px
-// .....
-// You get the idea. 
-
-
-// Note that the longest representation is 4 characters
-// Let's say there's queens at c3, c5 and e3
-// Qc3e5
-//   cannot beomce c3e since Qc3e1,
-//   and not c35 since Qc1e5,
-//   and not 3e5 or Q35, since Qe3e5,
-//   and not ce5 or Qe5, since Qc5e5,
-//   c3e5 is the minimum
-
-// Longest promotion is 3 characters
-fromFile toFile promotionPiece (e.g. with efB, disambiguates exf8=B from gxf8=B, exd8=B, exf8=N)
-```
-
-`/tablebase/`
 ```js
 // Information about theoretically perfect play:
-) // Unknown
+? // Unknown
 = // Draw
 > // side to move wins
 < // side to move loses
-
-// Examples:
-Qxf7)
-Qxf7=
-Qxf7|w
-Qxf7|b
 ```
 
+`/equivalent`:
+
+```rust
+id≡id
+
+// Two nodes can have different move orders but otherwise be equivalent
+// The "only" information that matters for a position is:
+set of moves since last 50 move counter reset
+where the pieces are on the board
+en passant
+castling
+
+// For example, two equivalent positions are:
+// a4 a5 b4
+// b4 a5 a4
+```
 
 ## file structure
-Files are organized from top node to bottom node. (Really an upside-down tree)  
+
+Files are organized from top node to bottom node. (Really an upside-down tree)
 The top node is the starting position.
 
-Each node is represented by a folder.  
-Each folder has "info.txt" and then child nodes/folders.  
+Each node is represented by a folder.
+Each folder has "info.txt" and then child nodes/folders.
 This way, you don't have to worry about folder limits.
 
-The moves are ordered by (8-1), (a-h), (piece from, piece to, alphabetical promotion)  
+The moves are ordered by (8-1), (a-h), (piece from, piece to, alphabetical promotion)
 The first instance of a node stays. Duplicate nodes shouldn't exist
 
 ## wait a minute
+
 _"Isn't this impossible?"_ Yes
 
-Note: Maybe in a billion trillion years it would be possible, since  
+Note: Maybe in a billion trillion years it would be possible, since
 `Number of states of atom in the universe > total chess positions > number of atoms in the universe`
 
-If this project gets over 1 GB, part 2 will be on a different repo. To be determined.  
+If this project gets over 1 GB, part 2 will be on a different repo. To be determined.
 But `1 GB = 1 000 000 000 bytes = 1 billion characters`, (text is very efficient), so I don't expect to reach that point soon.
---
+++ generate.py
@@ -0,0 +1,168 @@
+import chess
+import os
+from typing import List
+
+
+currentlayer = [("./", (), 0)]
+nextlayer = []
+nextID = 1
+boardata = {}
+
+def move_to_tuple(move: chess.Move):
+    return (move.from_square, move.to_square, move.promotion)
+
+
+# Returns only the necessary data needed to see if two positions are equivalent
+def to_equiv(board: chess.Board):
+    return (
+        board.pawns, board.knights, board.bishops, board.rooks, board.queens, board.kings,
+        tuple(sorted(board.move_stack, key=move_to_tuple)),
+        board.castling_rights,
+        None if not board.has_legal_en_passant() else board.ep_square)
+
+
+def sorted_legal_moves(board: chess.Board) -> List[chess.Move]:
+    return sorted(board.legal_moves, key=move_to_tuple)
+
+
+# This function is getting too big
+def make_node_and_update(board: chess.Board, root: int, nextID: int, path: str, stack: tuple[chess.Move]):
+    global boardata
+    global nextlayer
+    result = None
+    reason = None
+    moveresults = ""
+    equivs = ""
+    moves = sorted_legal_moves(board)
+    reasons = set()
+    index = nextID
+    root_path = os.path.join(path, str(root))
+    for move in moves:
+        board.push(move)
+
+        equiv = to_equiv(board)
+        if equiv in boardata:
+            equivs += f"\n{index}≡{boardata[equiv]}"
+            board.pop()
+            continue
+        else:
+            boardata[equiv] = index
+        stack2 = stack + (move,)
+        nextlayer.append((root_path, stack2, index))
+
+        if board.is_variant_win():
+            moveresults += "w"
+        elif board.is_variant_draw():
+            moveresults += "d"
+            if board.is_fifty_moves():
+                reasons.add("fifty moves")
+            elif board.is_stalemate():
+                reasons.add("stalemate")
+            elif board.is_insufficient_material():
+                reasons.add("insufficient material")
+            elif board.is_repetition():
+                reasons.add("threefold repetition")
+            else:
+                reasons.add("variant draw")
+        elif board.is_variant_loss():
+            moveresults += "l"
+        else:
+            try:
+                with open(os.path.join(path, str(root), str(index))) as f:
+                    moveresults += f.readline()
+                    mr = f.readline()
+                    if mr != "None":
+                        reasons.add(mr)
+            except OSError:
+                moveresults += "?"
+        board.pop()
+        index += 1
+
+    if "w" in moveresults:
+        result = "w"
+    elif "d" in moveresults:
+        result = "d"
+        if len(reasons) == 1:
+            reason = reasons.pop()
+        else:
+            reason = "dead position"
+    elif "l" in moveresults:
+        result = "l"
+    elif not moveresults:
+        if board.is_checkmate():
+            result = "l"
+        else:
+            result = "d"
+            reason = "stalemate"
+
+    txt = f"{result}\n{reason}\n#{root}\n{nextID}..{index - 1}\n{moveresults}\n{equivs}\n\n{board.fen()}\n"
+
+    try:
+        os.makedirs(os.path.join(path, str(root)))
+    except FileExistsError:
+        pass
+    with open(os.path.join(path, str(root), "info"), "w+") as f:
+        f.write(txt)
+    return index
+
+
+def update_from_layer_item(path: str, stack: tuple[chess.Move], nodeID: int):
+    global nextID
+
+    board = chess.Board()
+    for move in stack:
+        board.push(move)
+
+    nextID = make_node_and_update(board, nodeID, nextID, path, stack)
+    print(nodeID, nextID)
+
+
+def make_next_node():
+    global currentlayer
+    global nextlayer
+    if currentlayer:
+        path, stack, nodeID = currentlayer.pop(0)
+        update_from_layer_item(path, stack, nodeID)
+    elif nextlayer:
+        currentlayer = nextlayer
+        nextlayer = []
+        make_next_node()
+
+
+def make_next_layer():
+    global currentlayer
+    global nextlayer
+    # currentlayer: set of (str, tuple(), id)
+    # nextlayer: set of (str, tuple(move), id)
+    for (path, stack, nodeID) in currentlayer:
+        update_from_layer_item(path, stack, nodeID)
+    currentlayer = nextlayer
+    nextlayer = []
+    # print(currentlayer)
+
+def load_cache():
+    import pickle
+    global currentlayer
+    global nextlayer
+    global nextID
+    global boardata
+    try:
+        with open('cache.pickle', 'rb') as f:
+            currentlayer, nextlayer, nextID, boardata = pickle.load(f)
+    except FileNotFoundError:
+        pass
+
+def store_cache():
+    import pickle
+    global currentlayer
+    global nextlayer
+    global nextID
+    global boardata
+    with open('cache.pickle', 'wb') as f:
+        pickle.dump((currentlayer, nextlayer, nextID, boardata), f)
+
+
+if __name__ == "__main__":
+    load_cache()
+    make_next_node()
+    store_cache()
