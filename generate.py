4991027a0f7f777ba2d911e9122f1a3937d2d833
steven nguyen <nguyeste008@students.garlandisd.net>
2022-06-22 19:01:16 +0000

    fix Typing + Invalid code #6

--- generate.py
+++ generate.py
@@ -1,21 +1,38 @@
 import os
-from typing import List, Tuple
+from typing import Optional
 
 import chess
 
-currentlayer = [("./", (), 0)]
-nextlayer = []
-nextID = 1
-boardata = {}
+Version = int | str
+Id = int
+Path = str
+Node = tuple[Path, tuple[chess.Move], Id]
+Layer = list[Node]
+MoveTuple = tuple[int, int, int]
+BoardTuple = tuple[int, int, int, int, int, int, int, bool, int, chess.Square]
+Equiv = tuple[BoardTuple, tuple[BoardTuple], int]
 
-def move_to_tuple(move: chess.Move):
+version: Version = 1
+currentlayer: Layer = [("./", (), 0)]
+nextlayer: Layer = []
+nextID: int = 1
+boardata: dict[Equiv, tuple[Path, Id]] = {}
+
+def move_to_tuple(move: chess.Move) -> MoveTuple:
     return (move.from_square, move.to_square, move.promotion)
 
 
-# Returns a sorted tuple of moves since the last irrevers8ble move
+def board_to_tuple(board: chess.Board) -> BoardTuple:
+    return (
+        board.pawns, board.knights, board.bishops, board.rooks,
+        board.queens, board.kings, board.occupied_co[chess.WHITE],
+        board.turn, board.castling_rights, board.ep_square if board.has_legal_en_passant() else -1)
+
+# Returns a sorted tuple of moves since the last irreversible move
 # and the number of positions that add to the 100 ply counter but aren't actually reachable
-def _equiv_movedata(board: chess.Board):
-    move_stack = []
+def _equiv_movedata(board: chess.Board) -> tuple[tuple[BoardTuple], int]:
+    # Actually a list of reachable positions, previous moves don't matter
+    move_stack: list[chess.Board] = []
     irreversible_non_zeroing = 0
     board2 = chess.Board()
     for move in board.move_stack:
@@ -26,30 +43,195 @@ def _equiv_movedata(board: chess.Board):
             irreversible_non_zeroing += 1 + len(move_stack)
             move_stack = []
         else:
-            move_stack.append(move)
+            move_stack.append(board_to_tuple(board2.copy()))
         board2.push(move)
 
-    move_stack = tuple(sorted(board.move_stack, key=move_to_tuple))
-    return (move_stack, irreversible_non_zeroing)
-
+    move_stack.sort()
+    return (tuple(move_stack), irreversible_non_zeroing)
 
 
 # Returns only the necessary data needed to see if two positions are equivalent
-def to_equiv(board: chess.Board):
+def to_equiv(board: chess.Board) -> Equiv:
     move_stack, irreversible_non_zeroing = _equiv_movedata(board)
+    return (board_to_tuple(board), move_stack, irreversible_non_zeroing)
+
+
+# Symmetries
+def none(bt: BoardTuple) -> BoardTuple:
+    return bt
+
+
+def vmirr(bt: BoardTuple) -> BoardTuple:
     return (
-        board.pawns, board.knights, board.bishops, board.rooks, board.queens, board.kings,
-        board.castling_rights,
-        None if not board.has_legal_en_passant() else board.ep_square,
-        move_stack, irreversible_non_zeroing)
+        chess.flip_vertical(bt[0]),
+        chess.flip_vertical(bt[1]),
+        chess.flip_vertical(bt[2]),
+        chess.flip_vertical(bt[3]),
+        chess.flip_vertical(bt[4]),
+        chess.flip_vertical(bt[5]),
+        chess.flip_vertical(bt[6]),
+        bt[7],
+        chess.flip_vertical(bt[8]),
+        -1 if bt[9] == -1 else bt[9] ^ 0b111000,
+    )
 
 
-def sorted_legal_moves(board: chess.Board) -> List[chess.Move]:
+def hmirr(bt: BoardTuple) -> BoardTuple:
+    return (
+        chess.flip_horizontal(bt[0]),
+        chess.flip_horizontal(bt[1]),
+        chess.flip_horizontal(bt[2]),
+        chess.flip_horizontal(bt[3]),
+        chess.flip_horizontal(bt[4]),
+        chess.flip_horizontal(bt[5]),
+        chess.flip_horizontal(bt[6]),
+        bt[7],
+        chess.flip_horizontal(bt[8]),
+        -1 if bt[9] == -1 else bt[9] ^ 0b000111,
+    )
+
+
+def dmirr(bt: BoardTuple) -> BoardTuple:
+    return (
+        chess.flip_diagonal(bt[0]),
+        chess.flip_diagonal(bt[1]),
+        chess.flip_diagonal(bt[2]),
+        chess.flip_diagonal(bt[3]),
+        chess.flip_diagonal(bt[4]),
+        chess.flip_diagonal(bt[5]),
+        chess.flip_diagonal(bt[6]),
+        bt[7],
+        chess.flip_diagonal(bt[8]),
+        -1 if bt[9] == -1 else chess.square_file(bt[9]) << 3 + chess.square_rank(bt[9]),
+    )
+
+
+def d2mirr(bt: BoardTuple) -> BoardTuple:
+    return hmirr(dmirr(bt))
+
+
+def rot180(bt: BoardTuple) -> BoardTuple:
+    return hmirr(vmirr(bt))
+
+
+def rotc(bt: BoardTuple) -> BoardTuple:
+    return vmirr(dmirr(bt))
+
+
+def rotcc(bt: BoardTuple) -> BoardTuple:
+    return d2mirr(hmirr(bt))
+
+
+def switch_sides(bt: BoardTuple) -> BoardTuple:
+    return (
+        bt[0],
+        bt[1],
+        bt[2],
+        bt[3],
+        bt[4],
+        bt[5],
+        bt[6],
+        (bt[0] | bt[1] | bt[2] | bt[3] | bt[4] | bt[5] | bt[6]) ^ bt[7],
+        0, # Castling is ruined
+        bt[9]
+    )
+
+
+def transform_equiv(equiv: Equiv, transform, switch) -> Equiv:
+    if switch:
+        return (
+            transform(switch_sides(equiv[0])),
+            (transform(switch_sides(e)) for e in equiv[1]),
+            equiv[2]
+        )
+    else:
+        return (
+            switch_sides(equiv[0]),
+            (switch_sides(e) for e in equiv[1]),
+            equiv[2]
+        )
+
+
+
+def has_equiv(equiv: Equiv) -> Optional[tuple[Path, Id]]:
+    if equiv in boardata:
+        return boardata[equiv]
+
+    # No symmetries if castling*
+    pawns = False
+    boardtuples = (equiv[0],) + equiv[1]
+    for boardtuple in boardtuples:
+        if boardtuple[0] != 0:
+            pawns = True
+        elif boardtuple[7] != 0:
+            return None
+
+    rotated = transform_equiv(equiv, rot180, True)
+    if rotated in boardata:
+        return boardata[rotated]
+
+    vmirrored = transform_equiv(equiv, vmirr, True)
+    if vmirrored in boardata:
+        return boardata[vmirrored]
+
+    hmirrored = transform_equiv(equiv, hmirr, False)
+    if hmirrored in boardata:
+        return boardata[hmirrored]
+
+    if pawns:
+        return None
+
+    switched = transform_equiv(equiv, none, True)
+    if switched in boardata:
+        return boardata[switched]
+
+    rotated2 = transform_equiv(equiv, rot180, False)
+    if rotated2 in boardata:
+        return boardata[rotated2]
+
+    vmirrored2 = transform_equiv(equiv, vmirr, False)
+    if vmirrored2 in boardata:
+        return boardata[vmirrored2]
+
+    hmirrored2 = transform_equiv(equiv, hmirr, True)
+    if hmirrored2 in boardata:
+        return boardata[hmirrored2]
+
+    clockroted = transform_equiv(equiv, rotc, False)
+    if clockroted in boardata:
+        return boardata[clockroted]
+    cclocroted = transform_equiv(equiv, rotcc, False)
+    if cclocroted in boardata:
+        return boardata[cclocroted]
+    dmirrored = transform_equiv(equiv, dmirr, False)
+    if dmirrored in boardata:
+        return boardata[dmirrored]
+    d2mirrored = transform_equiv(equiv, d2mirr, False)
+    if d2mirrored in boardata:
+        return boardata[d2mirrored]
+    clockroted2 = transform_equiv(equiv, rotc, True)
+    if clockroted2 in boardata:
+        return boardata[clockroted2]
+    cclocroted2 = transform_equiv(equiv, rotcc, True)
+    if cclocroted2 in boardata:
+        return boardata[cclocroted2]
+    dmirrored2 = transform_equiv(equiv, dmirr, True)
+    if dmirrored2 in boardata:
+        return boardata[dmirrored2]
+    d2mirrored2 = transform_equiv(equiv, d2mirr, True)
+    if d2mirrored2 in boardata:
+        return boardata[d2mirrored2]
+    return None
+
+
+
+
+def sorted_legal_moves(board: chess.Board) -> list[chess.Move]:
     return sorted(board.legal_moves, key=move_to_tuple)
 
 
 # This function is getting too big
-def make_node_and_update(root: int, nextID: int, path: str, stack: Tuple[chess.Move, ...]):
+def make_node_and_update(root: int, nextID: int, path: str, stack: tuple[chess.Move, ...]):
     global boardata
     global nextlayer
 
@@ -74,10 +256,10 @@ def make_node_and_update(root: int, nextID: int, path: str, stack: Tuple[chess.M
 
         # check if equivalent
         equiv = to_equiv(board)
-        is_equiv = equiv in boardata
+        is_equiv = has_equiv(equiv)
         if is_equiv:
             # actual data
-            actual_path, actual_index = boardata[equiv]
+            actual_path, actual_index = is_equiv
 
             # update equivs
             equivs += f"\n{index}≡{actual_index}"
@@ -176,7 +358,7 @@ def make_node_and_update(root: int, nextID: int, path: str, stack: Tuple[chess.M
     return index
 
 
-def update_from_layer_item(path: str, stack: Tuple[chess.Move], nodeID: int):
+def update_from_layer_item(path: str, stack: tuple[chess.Move], nodeID: int):
     global nextID
     nextID = make_node_and_update(nodeID, nextID, path, stack)
     print(nodeID, nextID)
@@ -207,13 +389,32 @@ def make_next_layer():
 
 def load_cache():
     import pickle
+    import shutil
     global currentlayer
     global nextlayer
     global nextID
     global boardata
+    global version
     try:
+        d = None
         with open('cache.pickle', 'rb') as f:
-            currentlayer, nextlayer, nextID, boardata = pickle.load(f)
+            d = pickle.load(f)
+
+        v, c, nextl, nextI, b = d
+        if v != version:
+            print("Cache from outdated code, deleting cache")
+            os.remove("cache.pickle")
+            print("Deleting nodes")
+            shutil.rmtree("0/")
+            print("Regenerating nodes")
+            for _ in range(nextI):
+                make_next_node()
+        else:
+            currentlayer = c
+            nextlayer = nextl
+            nextID = nextI
+            boardata = b
+
     except FileNotFoundError:
         print("No cache found, starting from scratch")
 
@@ -224,12 +425,12 @@ def store_cache():
     global nextID
     global boardata
     with open('cache.pickle', 'wb') as f:
-        pickle.dump((currentlayer, nextlayer, nextID, boardata), f)
+        pickle.dump((version, currentlayer, nextlayer, nextID, boardata), f)
 
 
 def main():
     load_cache()
-    for _ in range(77):
+    for _ in range(17017):
         make_next_node()
     store_cache()
 
